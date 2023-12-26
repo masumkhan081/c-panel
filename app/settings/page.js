ed99fa83edb9483b6c1de9db354042e9b253b3af
masum khan <study.contents.of.masumkhan@gmail.com>
2023-12-26 23:08:35 +0600

    commited

---
+++ app/settings/page.js
@@ -0,0 +1,8 @@
+import EmailTemplate from '@/components/page-partials/setting/EmailTemplate'
+import React from 'react'
+
+export default function page() {
+  return (
+   <div className="bg-slate-200   w-full  h-full overflow-y-scroll  ">page-settings</div>
+  )
+}
